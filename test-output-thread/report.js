e2ceb7e1d4e00dfcf093cf1208ce53b71facdcbd
saikiran <saikiran@gmail.com>
2023-11-29 21:35:11 +0530

    first commit

---
+++ test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"3101fd83-b1c0-45c3-984e-598804e71dbf","feature":"Test Amazon page I want to use this template for my feature file","scenario":"Testing of SignIn Page","start":1701272578248,"group":1,"content":"","tags":"@all,","end":1701272589634,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
